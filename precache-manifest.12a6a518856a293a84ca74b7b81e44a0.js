80b580b64ed01301fba6b1b69ad597d875630bb9
Jason Mamed <mamed.jason@gmail.com>
2020-10-16 11:55:10 -0500

    Updates

---
+++ precache-manifest.12a6a518856a293a84ca74b7b81e44a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6da6548ff7dbb57c4aee96457ea09f4",
+    "url": "/jmamed-react/index.html"
+  },
+  {
+    "revision": "33a56912476568f8703e",
+    "url": "/jmamed-react/static/css/main.14f7c604.chunk.css"
+  },
+  {
+    "revision": "15617aabeb3f2f4f5f48",
+    "url": "/jmamed-react/static/js/2.89882c0e.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/jmamed-react/static/js/2.89882c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a56912476568f8703e",
+    "url": "/jmamed-react/static/js/main.dcaf030b.chunk.js"
+  },
+  {
+    "revision": "1a621d843cc70dfcb939",
+    "url": "/jmamed-react/static/js/runtime-main.4f20a034.js"
+  }
+]);
