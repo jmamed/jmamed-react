1c42abdb1f2c993e0908b05a9703eff319eb33a8
Jason Mamed <mamed.jason@gmail.com>
2021-08-05 19:29:02 -0500

    fix styling issue

--- src/navbar/NavigationBar.js
+++ src/navbar/NavigationBar.js
@@ -3,16 +3,11 @@ import { Nav, Navbar} from 'react-bootstrap'; //, Form, FormControl
 import '../App.css';
 
 export const NavigationBar = () => (
-  <Navbar expand="lg" className="app">
-    <header>
-      <h1 className="navTitle">Welcome to Jason Mamed's React App</h1>
-    </header>
-    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-    {/* <Form className="form-center">
-      <FormControl type="text" placeholder="Search" className="" />
-    </Form> */}
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto">
+  <Navbar expand="lg" bg="dark" variant="dark" className="app">
+    <Navbar.Brand href="/jmamed-react">Welcome to Jason Mamed's React App</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="me-auto">
         <Nav.Item><Nav.Link href="/jmamed-react">Home</Nav.Link></Nav.Item> 
         <Nav.Item><Nav.Link href="/tft">TFT WIP</Nav.Link></Nav.Item>
         <a className="button" href="https://jmamed.github.io/jmamed/"><span><h4 className="webButton">Angular Website</h4></span></a>
